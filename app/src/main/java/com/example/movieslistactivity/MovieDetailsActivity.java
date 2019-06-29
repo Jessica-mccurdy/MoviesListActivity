2dbc6885185d4e4ed9a1b50233c9eb345aea145b
jessica-mccurdy <jessica.mccurdy@yale.edu>
2019-06-28 17:42:23 -0700

    Added picture to details page

--- app/src/main/java/com/example/movieslistactivity/MovieDetailsActivity.java
+++ app/src/main/java/com/example/movieslistactivity/MovieDetailsActivity.java
@@ -3,9 +3,14 @@ package com.example.movieslistactivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.movieslistactivity.models.Movie;
 
 import org.parceler.Parcels;
@@ -17,7 +22,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
     // the view objects
     TextView tvTitle;
     TextView tvOverview;
+    ImageView ivPosterImage;
     RatingBar rbVoteAverage;
+    String imageUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +34,12 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPosterImage = (ImageView) findViewById(R.id.ivPosterImage);
+
 
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        imageUrl = "https://image.tmdb.org/t/p/w780" + movie.getBackdropPath();
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
 
         // set the title and overview
@@ -39,5 +49,17 @@ public class MovieDetailsActivity extends AppCompatActivity {
         // vote average is 0..10, convert to 0..5 by dividing by 2
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage = voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
+
+
+
+
+        // load images using glide
+        Glide.with(getApplicationContext())
+                .load(imageUrl)
+                .apply(new RequestOptions()
+                        .transforms(new CenterCrop(), new RoundedCorners(20))
+                        .placeholder(R.drawable.flicks_backdrop_placeholder)
+                        .error(R.drawable.flicks_backdrop_placeholder))
+                .into(ivPosterImage);
     }
 }
